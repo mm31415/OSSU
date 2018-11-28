85add5e60abd3d5b037b77a98ce0a5ea0794892d
mm31415 <martinez.m31415@gmail.com>
2018-11-28 12:37:54 -0500

    Still trying to squash the large file history, Percolation needs to be tested

--- algos1Prince/unionFind/Client.java
+++ algos1Prince/unionFind/Client.java
@@ -1,9 +1,26 @@
+/*
+  javac WeightPathQUF.java
+  javac -cp .:../algos4.jar Client.java
+  java -cp .:../algos4.jar Client < tinyUF.txt
+
+  When two classes are in the same directory you do not need to import
+  the class.  This is why WeightPathQUF can be used without an import
+  statement.
+
+  -cp stands for -classpath
+  . stands for start in this directory
+  : is used to seperate classpaths
+  < filename.type  means the file will be used as the standard inputgit 
+*/
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+
 public class Client {
   public static void main(String[] args) {
     int N = StdIn.readInt();
-    UF uf = new UF(N);
+    WeightPathQUF uf = new WeightPathQUF(N);
 
-    while (!StdIn.empty()) {
+    while (!StdIn.isEmpty()) {
       int p = StdIn.readInt();
       int q = StdIn.readInt();
 
---
+++ algos1Prince/unionFind/percolation/Percolation.java
@@ -0,0 +1,79 @@
+import edu.princeton.cs.algs4.WeightedQuickUnionUF;
+
+public class Percolation {
+  private boolean[][] grid;
+  private WeightedQuickUnionUF uf;
+  private int openSites;
+  private int virtualTop;
+  private int virtualBottom;
+
+  public Percolation(int n) {
+    if (n <= 0) throw new IllegalArgumentException("n must be greater than 0");
+    grid = new boolean[n][n];
+    openSites = 0;
+    virtualTop = 0;
+    virtualBottom = n*n + 1;
+    uf = new WeightedQuickUnionUF(n*n + 2);
+  }
+
+  public void open(int row, int col) {
+    validates(row, col);
+
+    if (isFull(row, col)) {
+      grid[row - 1][col - 1] = true;
+      openSites += 1;
+
+      int n = grid[0].length;
+      int p = gridValue(row, col, n);
+
+      if (row == n) {
+        uf.union(p, virtualBottom);
+      } else if (row == 1) {
+          uf.union(p, virtualTop);
+      } else if (row + 1 <= n && isOpen(row + 1, col)) {
+          uf.union(p, gridValue(row + 1, col, n));
+      } else if (row - 1 >= 1 && isOpen(row - 1, col)) {
+          uf.union(p, gridValue(row - 1, col, n));
+      } else if (col + 1 <= n && isOpen(row, col + 1)) {
+          uf.union(p, gridValue(row, col + 1, n));
+      } else if (col - 1 >= 1 && isOpen(row, col - 1)) {
+          uf.union(p, gridValue(row, col - 1, n));
+      }
+    }
+  }
+
+  public boolean isOpen(int row, int col) {
+    validates(row, col);
+    return grid[row - 1][col - 1];
+  }
+
+  public boolean isFull(int row, int col) {
+    validates(row, col);
+    return !grid[row - 1][col - 1];
+  }
+
+  public int numberOfOpenSites() {
+    return openSites;
+  }
+
+  public boolean percolates() {
+    return uf.connected(virtualTop, virtualBottom);
+  }
+
+  private void validates(int row, int col, int n) {
+    int n = grid[0].length;
+
+    if (row < 1 || row > n) {
+      throw new IllegalArgumentException("row " + row + " not between 1 and " + n);
+    } else if (col < 1 || col > n) {
+      throw new IllegalArgumentException("col " + col + " not between 1 and " + n);
+    }
+  }
+
+  private int gridValue(int row, int col, int n) {
+    return n * (row - 1) + col;
+  }
+
+  public static void main(String[] args) {
+  }
+}
