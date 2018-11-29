6bc1a64d76894f23d825d55f9723cb81fbba9879
mm31415 <martinez.m31415@gmail.com>
2018-11-28 19:45:36 -0500

    PercolationStats mostly finished, need to figure out what confidenceLo and confidenceHi are supposed to do

---
+++ algos1Prince/unionFind/percolation/PercolationStats.java
@@ -0,0 +1,68 @@
+import edu.princeton.cs.algs4.StdRandom;
+import edu.princeton.cs.algs4.StdStats;
+
+public class PercolationStats {
+  private int n;
+  private int trials;
+  private Percolation perc;
+  private double[] threshold;
+
+  public PercolationStats(int n, int trials) {
+    if (n <= 0 || trials <= 0) {
+      throw new IllegalArgumentException("n or trials is <= 0");
+    }
+    this.n = n;
+    this.trials = trials;
+    threshold = new double[trials];
+
+    runTests();
+  }
+
+  private void runTests() {
+    for (int i = 0; i < trials; i++) {
+      perc = new Percolation(n);
+      while (!perc.percolates()) {
+        perc.open(
+          StdRandom.uniform(1, n + 1), StdRandom.uniform(1, n + 1)
+        );
+      }
+
+      threshold[i] = (double) perc.numberOfOpenSites() / (n * n);
+    }
+  }
+
+  public double mean() {
+    return StdStats.mean(threshold);
+  }
+
+  public double stddev() {
+    return StdStats.stddev(threshold);
+  }
+
+  public double confidencLo() {
+    return 1.0;
+  }
+
+  public double confidenceHi() {
+    return 1.0;
+  }
+
+  public static void main(String[] args) {
+    int n, trials;
+    if (args.length == 2) {
+      n = Integer.parseInt(args[0]);
+      trials = Integer.parseInt(args[1]);
+    } else {
+      throw new IllegalArgumentException("missing info");
+    }
+
+    if (n <= 0 || trials <= 0) {
+      throw new IllegalArgumentException("values must be greater than 0");
+    }
+
+    PercolationStats pStat = new PercolationStats(n, trials);
+
+    System.out.println("mean = " + pStat.mean());
+    System.out.println("stddev = " + pStat.stddev());
+  }
+}
